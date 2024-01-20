253a22bdd845f8274089eaadeb0989494463f775
Scott Ellis <scott-ellis@idexx.com>
2024-01-20 15:29:54 -0500

    Use -28 dtb version for newer display

--- recipes-bsp/u-boot-scr/files/boot.cmd
+++ recipes-bsp/u-boot-scr/files/boot.cmd
@@ -4,7 +4,7 @@ setenv mmcroot /dev/mmcblk0p2 rw
 setenv mmcrootfstype ext4 rootwait
 setenv console ttyS0,115200n8
 setenv bootargs console=${console} root=${mmcroot} rootfstype=${mmcrootfstype}
-setenv fdtfile logicpd-torpedo-37xx-devkit.dtb
+setenv fdtfile logicpd-torpedo-37xx-devkit-28.dtb
 load mmc ${bootpart} ${fdtaddr} ${bootdir}/${fdtfile}
 load mmc ${bootpart} ${loadaddr} ${bootdir}/zImage
 bootz ${loadaddr} - ${fdtaddr}
